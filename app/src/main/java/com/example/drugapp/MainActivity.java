ba913d7ec0f400c3e19d829e78cc22eb6cc1986a
Mouayad <mouayad.karimeh@fh-bielefeld.de>
2023-01-05 10:41:30 +0100

    MainActivity.java: setOnClickListener for Button add

--- app/src/main/java/com/example/drugapp/MainActivity.java
+++ app/src/main/java/com/example/drugapp/MainActivity.java
@@ -2,16 +2,30 @@ package com.example.drugapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv ;
+    Button add ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
 
+    protected void initViews(){
+        tv = (TextView) findViewById(R.id.textView_hello);
+        add = (Button) findViewById(R.id.button_add);
+        add.setOnClickListener(View->onAddButton());
+    }
 
-       // to do code.
+    protected void onAddButton(){
+        Intent intentToAddActivity = new Intent(this,AddActivity.class );
+        startActivity(intentToAddActivity);
     }
 }
