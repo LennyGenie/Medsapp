417aca9ca084b1152963e6aa9e79c6b0682bafab
Mouayad <mouayad.karimeh@fh-bielefeld.de>
2023-01-12 02:50:04 +0100

    Delete funktioniert nicht

--- app/src/main/java/com/example/drugapp/MainActivity.java
+++ app/src/main/java/com/example/drugapp/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.drugapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
+
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,10 +11,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
-import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
     EditText etName , etAmount , etFrequency ;
@@ -29,81 +29,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ayctivity_main2);
-        initViews();
-        dataBaceHelper = new DataBaceHelper(this);
 
-        medDataArrayAdapter = new ArrayAdapter<MedData>(this, android.R.layout.simple_list_item_1, dataBaceHelper.getEvryone());
-        lv.setAdapter(medDataArrayAdapter);
-    }
-
-    protected void initViews(){
         etName = (EditText) findViewById(R.id.name);
         etAmount = (EditText) findViewById(R.id.amount);
         etFrequency = (EditText) findViewById(R.id.frequency);
         addButton = (Button) findViewById(R.id.add);
         overviewButton = (Button) findViewById(R.id.overview);
         lv = findViewById(R.id.lv);
+        dataBaceHelper = new DataBaceHelper(MainActivity.this);
 
-        dataBaceHelper = new DataBaceHelper(this);
-        medDataArrayAdapter = new ArrayAdapter<MedData>(this, android.R.layout.simple_list_item_1, dataBaceHelper.getEvryone());
-        lv.setAdapter(medDataArrayAdapter);
+        showMedDataOnListView(dataBaceHelper);
 
-        addButton.setOnClickListener(View->onAddButton());
-        overviewButton.setOnClickListener(View->onOverviewButton());
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MedData medData;
+                try {
+                    medData = new MedData(-1, etName.getText().toString(),
+                            Integer.parseInt(etAmount.getText().toString()),
+                            Integer.parseInt(etFrequency.getText().toString()));
+                    Toast.makeText(MainActivity.this, medData.toString(), Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    medData = new MedData(-1, "", 0, 0);
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+
+                DataBaceHelper dataBaceHelper = new DataBaceHelper(MainActivity.this);
+                boolean success = dataBaceHelper.addOne(medData);
+                Toast.makeText(MainActivity.this, "Success" + success, Toast.LENGTH_SHORT).show();
+                showMedDataOnListView(dataBaceHelper);
+
+            }
+        });
+
+
+        overviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DataBaceHelper dataBaceHelper = new DataBaceHelper(MainActivity.this);
+                showMedDataOnListView(dataBaceHelper);
+            }
+        });
+
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 MedData clickedMedData = (MedData) parent.getItemAtPosition(position);
                 dataBaceHelper.deleteOne(clickedMedData);
                 showMedDataOnListView(dataBaceHelper);
-                Toast.makeText(MainActivity.this, "Delete"+ clickedMedData.toString(), Toast.LENGTH_SHORT).show();
-
-
+                Toast.makeText(MainActivity.this, "Delete" + clickedMedData.toString(), Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
 
 
-
-    protected void onAddButton() {
-        MedData medData;
-        try {
-            medData = new MedData(-1, etName.getText().toString(),
-                    Integer.parseInt(etAmount.getText().toString()),
-                    Integer.parseInt(etFrequency.getText().toString()));
-            Toast.makeText(this, medData.toString(), Toast.LENGTH_SHORT).show();
-        }
-        catch (Exception e){
-            medData = new MedData(-1, "",0,0);
-            Toast.makeText(this,"Error", Toast.LENGTH_SHORT).show();
-        }
-
-        DataBaceHelper dataBaceHelper = new DataBaceHelper(this);
-        boolean success = dataBaceHelper.addOne(medData);
-        Toast.makeText(this, "Success" + success , Toast.LENGTH_SHORT).show();
-        showMedDataOnListView(dataBaceHelper);
-    }
-
-
-
-
-
-
-
-    protected void onOverviewButton() {
-        DataBaceHelper dataBaceHelper = new DataBaceHelper(this);
-        showMedDataOnListView(dataBaceHelper);
-    }
-
-
-
-
-    protected void showMedDataOnListView(DataBaceHelper dataBaceHelper2)
-    {
-        medDataArrayAdapter = new ArrayAdapter<MedData>(this, android.R.layout.simple_list_item_1, dataBaceHelper.getEvryone());
+    private void showMedDataOnListView(DataBaceHelper dataBaceHelper) {
+        medDataArrayAdapter = new ArrayAdapter<MedData>(MainActivity.this, android.R.layout.simple_list_item_1, dataBaceHelper.getEvryone());
         lv.setAdapter(medDataArrayAdapter);
     }
-
 
 
 
